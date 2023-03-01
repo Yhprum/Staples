eabcfd100cf2ce481b04b651e8bd43b8817cbd15
Ryan Murphy <ryanmurphy@gatech.edu>
2023-02-28 20:21:01 -0500

    add database

--- src/routes/add/+page.server.ts
+++ src/routes/add/+page.server.ts
@@ -1,7 +1,9 @@
+import { db } from "$lib/server/db";
 import { getSession } from "$lib/server/session";
 import type { Actions } from "./$types";
 
-interface CouponResponse {
+interface CouponSuccessResponse {
+  couponValid: true;
   code: string;
   couponType: string;
   description: string;
@@ -13,44 +15,94 @@ interface CouponResponse {
   checkoutOnly: boolean;
   isMembershipBenefit: boolean;
   couponQualified: boolean;
-  couponValid: boolean;
   autoAddCoupon: boolean;
   rewardsCoupon: boolean;
   couponCodeDisplayable: boolean;
   couponRemovable: boolean;
-  basketLevelCoupon: boolean
+  basketLevelCoupon: boolean;
 }
+
+interface CouponFailureResponse {
+  couponValid: false;
+  code: string;
+  errorReason: string;
+  errorMessage: string;
+  discountAmount: { currency: string };
+  isMembershipBenefit: boolean;
+  couponQualified: boolean;
+  autoAddCoupon: boolean;
+  rewardsCoupon: boolean;
+  couponCodeDisplayable: boolean;
+  couponRemovable: boolean;
+  basketLevelCoupon: boolean;
+}
+
+type CouponResponse = CouponSuccessResponse | CouponFailureResponse;
 
 export const actions = {
   default: async ({ request }) => {
     const form = await request.formData();
     const couponCode = form.get("coupon");
-    if (!couponCode || (couponCode instanceof String && couponCode.trim().length !== 16)) {
+    const expiration = form.get("expiration");
+    if (typeof couponCode !== "string" || couponCode.trim().length !== 16) {
       return { success: false, error: "Incorrect Coupon Format: must be a 16 digit code" };
     }
-    const cartId = await getSession();
-    console.log(cartId);
+    if (typeof expiration !== "string") {
+      return { success: false, error: "Invalid request" };
+    }
 
-    if (!cartId) return { success: false, error: "An unexpected error occurred, contact /u/Arceus919" };
+    const cartId = await getSession();
+    if (!cartId) return { success: false, error: "An unexpected error occurred" };
 
     const options = {
       method: "POST",
       body: JSON.stringify({ coupons: [couponCode] }),
       headers: { "Content-Type": "application/json" },
     };
-    let res: Response = await fetch(`https://www.staples.com/cc/api/checkout/${cartId}/coupon`, options);
+    let res = await fetch(`https://www.staples.com/cc/api/checkout/${cartId}/coupon`, options);
     let data = await res.json();
 
-    console.log("coupon response");
     const coupon: CouponResponse = data.cartInfo.coupons[0];
-    const tries = data.cartInfo.invalidCouponTries;
     console.log(coupon);
-    console.log(tries);
+    const tries: number = data.cartInfo.invalidCouponTries;
+    console.log("Invalid tries: " + tries);
+
+    if (coupon.couponValid) {
+      const requirement = coupon.description?.match(/\d+/g) ?? [];
+      // console.log(JSON.stringify({
+      //   code: coupon.code,
+      //   description: coupon.description,
+      //   discount: coupon.couponValue,
+      //   requirement: parseInt(requirement[1]),
+      //   expiration,
+      // }));
+      const couponType = await db.couponType.upsert({
+        where: {
+          discount_requirement: {
+            discount: coupon.couponValue,
+            requirement: parseInt(requirement[1]),
+          }
+        },
+        update: {},
+        create: {
+          discount: coupon.couponValue,
+          requirement: parseInt(requirement[1]),
+          description: coupon.description,
+        }
+      });
+      const c = await db.coupon.create({
+        data: {
+          code: coupon.code,
+          expiration: new Date(expiration),
+          couponTypeDiscount: couponType.discount,
+          couponTypeRequirement: couponType.requirement
+        }
+      });
+    }
     
     return {
       success: coupon.couponValid,
-      message: coupon.description,
-      error: coupon.errorMessage
+      message: coupon.couponValid ? `Coupon added: ${coupon.description}` : coupon.errorMessage,
     };
   }
 } satisfies Actions;
--- src/routes/add/+page.svelte
+++ src/routes/add/+page.svelte
@@ -3,6 +3,7 @@
 
   // export let data: PageData;
   export let form: ActionData;
+  const inputDate = new Date(Date.now() + 1000 * 60 * 60 * 24 * 5).toISOString().slice(0, 10);
 </script>
 
 <div class="container mx-auto mt-7 flex flex-col">
@@ -13,6 +14,10 @@
           16-digit Coupon Code
           <input name="coupon" maxlength="16" class="text-black" />
         </label>
+        <label class="m-auto">
+          Expiration Date
+          <input name="expiration" class="text-black mt-2" type="date" value={inputDate} />
+        </label>
         <div class="flex flex-row">
           <a href="/" class="button border border-slate-400 mt-2 ml-auto">Cancel</a>
           <button class="button bg-slate-400 mt-2 ml-2">Add Coupon</button>
@@ -20,7 +25,7 @@
       </div>
     </form>
   </div>
-  {#if form?.error}
-    <div class="text-center text-sm">{form?.error}</div>
+  {#if form}
+    <div class="text-center text-sm">{form.message}</div>
   {/if}
 </div>
